0dffe740203549d346d7f18da552ea356a7dc23f
Peien Liu <simonlpe.03@gmail.com>
2025-10-22 19:59:42 -0400

    Fix b tree and build henn

--- bthenn/bthenn.h
+++ bthenn/bthenn.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <algorithm>
 #include <iostream>
+#include <string>
  
 using namespace std;
 using namespace hnswlib;
@@ -19,13 +20,20 @@ namespace btree_henn
     {
     public:
         // Array of keys
-        T keys[ORDER - 1];
+        pair<T, int> keys[ORDER - 1];
         // Array of child pointers
         BTreeNode *children[ORDER];
         // Current number of keys
         int n;
         // True if leaf node, false otherwise
         bool leaf;
+ 
+        // Keep track of all henn data in subtree
+        vector<int> henn_indices;
+        float* henn_data;
+ 
+        // store HENN index for this node
+        unique_ptr<HierarchicalNSW<float>> henn_index;
  
         BTreeNode(bool isLeaf = true) : n(0), leaf(isLeaf)
         {
@@ -40,56 +48,66 @@ namespace btree_henn
     {
     private:
         BTreeNode<T, ORDER> *root; // Pointer to root node
+        float* henn_data;      // HENN data for all elements
+        int dim;              // Dimension of data points
+        int n;                // Number of elements
  
         // Function to split a full child node
         void splitChild(BTreeNode<T, ORDER> *x, int i)
         {
             BTreeNode<T, ORDER> *y = x->children[i];
             BTreeNode<T, ORDER> *z = new BTreeNode<T, ORDER>(y->leaf);
-            z->n = ORDER / 2 - 1;
  
-            for (int j = 0; j < ORDER / 2 - 1; j++)
-                z->keys[j] = y->keys[j + ORDER / 2];
+            int mid = ORDER / 2;
+            z->n = ORDER - 1 - mid;
+ 
+            // Copy the last half of keys to new node
+            for (int j = 0; j < z->n; j++)
+                z->keys[j] = y->keys[j + mid];
  
             if (!y->leaf)
             {
-                for (int j = 0; j < ORDER / 2; j++)
-                    z->children[j] = y->children[j + ORDER / 2];
+                // Copy children pointers
+                for (int j = 0; j <= z->n; j++)
+                    z->children[j] = y->children[j + mid];
             }
  
-            y->n = ORDER / 2 - 1;
+            y->n = mid - 1;
  
+            // Make room for new child in parent
             for (int j = x->n; j >= i + 1; j--)
                 x->children[j + 1] = x->children[j];
  
             x->children[i + 1] = z;
  
+            // Make room for promoted key in parent
             for (int j = x->n - 1; j >= i; j--)
                 x->keys[j + 1] = x->keys[j];
  
-            x->keys[i] = y->keys[ORDER / 2 - 1];
+            // Promote the middle key
+            x->keys[i] = y->keys[mid - 1];
             x->n = x->n + 1;
         }
  
         // Function to insert a key in a non-full node
-        void insertNonFull(BTreeNode<T, ORDER> *x, T k)
+        void insertNonFull(BTreeNode<T, ORDER> *x, T k, int index)
         {
             int i = x->n - 1;
  
             if (x->leaf)
             {
-                while (i >= 0 && k < x->keys[i])
+                while (i >= 0 && k < x->keys[i].first)
                 {
                     x->keys[i + 1] = x->keys[i];
                     i--;
                 }
  
-                x->keys[i + 1] = k;
+                x->keys[i + 1] = make_pair(k, index);
                 x->n = x->n + 1;
             }
             else
             {
-                while (i >= 0 && k < x->keys[i])
+                while (i >= 0 && k < x->keys[i].first)
                     i--;
  
                 i++;
@@ -97,10 +115,10 @@ namespace btree_henn
                 {
                     splitChild(x, i);
  
-                    if (k > x->keys[i])
+                    if (k > x->keys[i].first)
                         i++;
                 }
-                insertNonFull(x->children[i], k);
+                insertNonFull(x->children[i], k, index);
             }
         }
  
@@ -112,7 +130,7 @@ namespace btree_henn
             {
                 if (!x->leaf)
                     traverse(x->children[i]);
-                cout << " " << x->keys[i];
+                cout << " " << x->keys[i].first;
             }
  
             if (!x->leaf)
@@ -123,10 +141,10 @@ namespace btree_henn
         BTreeNode<T, ORDER> *search(BTreeNode<T, ORDER> *x, T k)
         {
             int i = 0;
-            while (i < x->n && k > x->keys[i])
+            while (i < x->n && k > x->keys[i].first)
                 i++;
  
-            if (i < x->n && k == x->keys[i])
+            if (i < x->n && k == x->keys[i].first)
                 return x;
  
             if (x->leaf)
@@ -135,193 +153,11 @@ namespace btree_henn
             return search(x->children[i], k);
         }
  
-        // Function to find the predecessor
-        T getPredecessor(BTreeNode<T, ORDER> *node, int idx)
-        {
-            BTreeNode<T, ORDER> *current = node->children[idx];
-            while (!current->leaf)
-                current = current->children[current->n];
-            return current->keys[current->n - 1];
-        }
- 
-        // Function to find the successor
-        T getSuccessor(BTreeNode<T, ORDER> *node, int idx)
-        {
-            BTreeNode<T, ORDER> *current = node->children[idx + 1];
-            while (!current->leaf)
-                current = current->children[0];
-            return current->keys[0];
-        }
- 
-        // Function to fill child node
-        void fill(BTreeNode<T, ORDER> *node, int idx)
-        {
-            if (idx != 0 && node->children[idx - 1]->n >= ORDER / 2)
-                borrowFromPrev(node, idx);
-            else if (idx != node->n && node->children[idx + 1]->n >= ORDER / 2)
-                borrowFromNext(node, idx);
-            else
-            {
-                if (idx != node->n)
-                    merge(node, idx);
-                else
-                    merge(node, idx - 1);
-            }
-        }
- 
-        // Function to borrow from previous sibling
-        void borrowFromPrev(BTreeNode<T, ORDER> *node, int idx)
-        {
-            BTreeNode<T, ORDER> *child = node->children[idx];
-            BTreeNode<T, ORDER> *sibling = node->children[idx - 1];
- 
-            for (int i = child->n - 1; i >= 0; --i)
-                child->keys[i + 1] = child->keys[i];
- 
-            if (!child->leaf)
-            {
-                for (int i = child->n; i >= 0; --i)
-                    child->children[i + 1] = child->children[i];
-            }
- 
-            child->keys[0] = node->keys[idx - 1];
- 
-            if (!child->leaf)
-                child->children[0] = sibling->children[sibling->n];
- 
-            node->keys[idx - 1] = sibling->keys[sibling->n - 1];
- 
-            child->n += 1;
-            sibling->n -= 1;
-        }
- 
-        // Function to borrow from next sibling
-        void borrowFromNext(BTreeNode<T, ORDER> *node, int idx)
-        {
-            BTreeNode<T, ORDER> *child = node->children[idx];
-            BTreeNode<T, ORDER> *sibling = node->children[idx + 1];
- 
-            child->keys[child->n] = node->keys[idx];
- 
-            if (!child->leaf)
-                child->children[child->n + 1] = sibling->children[0];
- 
-            node->keys[idx] = sibling->keys[0];
- 
-            for (int i = 1; i < sibling->n; ++i)
-                sibling->keys[i - 1] = sibling->keys[i];
- 
-            if (!sibling->leaf)
-            {
-                for (int i = 1; i <= sibling->n; ++i)
-                    sibling->children[i - 1] = sibling->children[i];
-            }
- 
-            child->n += 1;
-            sibling->n -= 1;
-        }
- 
-        // Function to merge two nodes
-        void merge(BTreeNode<T, ORDER> *node, int idx)
-        {
-            BTreeNode<T, ORDER> *child = node->children[idx];
-            BTreeNode<T, ORDER> *sibling = node->children[idx + 1];
- 
-            child->keys[ORDER / 2 - 1] = node->keys[idx];
- 
-            for (int i = 0; i < sibling->n; ++i)
-                child->keys[i + ORDER / 2] = sibling->keys[i];
- 
-            if (!child->leaf)
-            {
-                for (int i = 0; i <= sibling->n; ++i)
-                    child->children[i + ORDER / 2] = sibling->children[i];
-            }
- 
-            for (int i = idx + 1; i < node->n; ++i)
-                node->keys[i - 1] = node->keys[i];
- 
-            for (int i = idx + 2; i <= node->n; ++i)
-                node->children[i - 1] = node->children[i];
- 
-            child->n += sibling->n + 1;
-            node->n--;
- 
-            delete sibling;
-        }
- 
-        // Function to remove a key from a non-leaf node
-        void removeFromNonLeaf(BTreeNode<T, ORDER> *node, int idx)
-        {
-            T k = node->keys[idx];
- 
-            if (node->children[idx]->n >= ORDER / 2)
-            {
-                T pred = getPredecessor(node, idx);
-                node->keys[idx] = pred;
-                remove(node->children[idx], pred);
-            }
-            else if (node->children[idx + 1]->n >= ORDER / 2)
-            {
-                T succ = getSuccessor(node, idx);
-                node->keys[idx] = succ;
-                remove(node->children[idx + 1], succ);
-            }
-            else
-            {
-                merge(node, idx);
-                remove(node->children[idx], k);
-            }
-        }
- 
-        // Function to remove a key from a leaf node
-        void removeFromLeaf(BTreeNode<T, ORDER> *node, int idx)
-        {
-            for (int i = idx + 1; i < node->n; ++i)
-                node->keys[i - 1] = node->keys[i];
- 
-            node->n--;
-        }
- 
-        // Function to remove a key from the tree
-        void remove(BTreeNode<T, ORDER> *node, T k)
-        {
-            int idx = 0;
-            while (idx < node->n && node->keys[idx] < k)
-                ++idx;
- 
-            if (idx < node->n && node->keys[idx] == k)
-            {
-                if (node->leaf)
-                    removeFromLeaf(node, idx);
-                else
-                    removeFromNonLeaf(node, idx);
-            }
-            else
-            {
-                if (node->leaf)
-                {
-                    cout << "The key " << k << " is not present in the tree\n";
-                    return;
-                }
- 
-                bool flag = ((idx == node->n) ? true : false);
- 
-                if (node->children[idx]->n < ORDER / 2)
-                    fill(node, idx);
- 
-                if (flag && idx > node->n)
-                    remove(node->children[idx - 1], k);
-                else
-                    remove(node->children[idx], k);
-            }
-        }
- 
     public:
         BTree() { root = new BTreeNode<T, ORDER>(true); }
  
         // Function to insert a key in the tree
-        void insert(T k)
+        void insert(T k, int index)
         {
             if (root->n == ORDER - 1)
             {
@@ -329,10 +165,69 @@ namespace btree_henn
                 s->children[0] = root;
                 root = s;
                 splitChild(s, 0);
-                insertNonFull(s, k);
+                insertNonFull(s, k, index);
             }
             else
-                insertNonFull(root, k);
+                insertNonFull(root, k, index);
+        }
+ 
+        void build_henn_index(float* data, int num_elements, int dimension)
+        {
+            henn_data = data;
+            dim = dimension;
+            n = num_elements;
+            build_henn_recursive(root);
+        }
+ 
+        void build_henn_recursive(BTreeNode<T, ORDER>* node)
+        {
+            if (node == nullptr) return;
+ 
+            // If leaf, collect keys' data
+            for (int i = 0; i < node->n; i++)
+            {
+                node->henn_indices.push_back(node->keys[i].second);
+            }
+            if (!node->leaf)
+            {
+                // For non-leaf nodes, recursively build HENN indexes for children
+                for (int i = 0; i <= node->n; i++) {
+                    build_henn_recursive(node->children[i]);
+                }
+               
+                // Collect indices from all children
+                for (int i = 0; i <= node->n; i++) {
+                    if (node->children[i]) {
+                        node->henn_indices.insert(
+                            node->henn_indices.end(),
+                            node->children[i]->henn_indices.begin(),
+                            node->children[i]->henn_indices.end()
+                        );
+                    }
+                }
+            }
+            // Allocate memory for the node's HENN data
+            node->henn_data = new float[dim * node->henn_indices.size()];
+           
+            // Copy data for each index in this node
+            for (size_t j = 0; j < node->henn_indices.size(); j++) {
+                int idx = node->henn_indices[j];
+                // Copy the feature vector for this index
+                for (int d = 0; d < dim; d++) {
+                    node->henn_data[j * dim + d] = henn_data[idx * dim + d];
+                }
+            }
+           
+            // Create HENN index for this node if it has enough points
+            // if (node->henn_indices.size() > 0) {
+            //     node->henn_index = make_unique<HierarchicalNSW<float>>(new L2Space(dim));
+            //     node->henn_index->initNewIndex(node->henn_indices.size(), 16, 200);
+               
+            //     // Add each point to the index
+            //     for (size_t j = 0; j < node->henn_indices.size(); j++) {
+            //         node->henn_index->addPoint(&node->henn_data[j * dim], j);
+            //     }
+            // }
         }
  
         // Function to traverse the tree
@@ -348,27 +243,82 @@ namespace btree_henn
             return (root == nullptr) ? nullptr : search(root, k);
         }
  
-        // Function to remove a key from the tree
-        void remove(T k)
+        // Function to get the root node (for testing and debugging)
+        BTreeNode<T, ORDER> *getRoot() const
         {
-            if (!root)
-            {
-                cout << "The tree is empty\n";
+            return root;
+        }
+ 
+        // Function to print detailed tree structure
+        void printTreeStructure() const
+        {
+            cout << "=== B-Tree Structure (ORDER=" << ORDER << ") ===" << endl;
+            if (root == nullptr) {
+                cout << "Tree is empty" << endl;
                 return;
             }
+            printTreeStructureHelper(root, 0, "Root: ");
+            cout << endl;
+        }
  
-            remove(root, k);
+    private:
+        // Helper function to print tree structure recursively
+        void printTreeStructureHelper(BTreeNode<T, ORDER>* node, int level, const string& prefix) const
+        {
+            if (node == nullptr) return;
+           
+            // Print indentation for current level
+            for (int i = 0; i < level; i++) {
+                cout << "    ";
+            }
+           
+            cout << prefix;
+           
+            // Print node type
+            if (node->leaf) {
+                cout << "[LEAF] ";
+            } else {
+                cout << "[INTERNAL] ";
+            }
+           
+            // Print keys in this node
+            cout << "Keys(" << node->n << "): [";
+            for (int i = 0; i < node->n; i++) {
+                cout << node->keys[i].first << " (Index: " << node->keys[i].second << ")";
+                if (i < node->n - 1) cout << ", ";
+            }
+            cout << "]";
+            cout << "; HENN Indices: {";
+            for (size_t i = 0; i < node->henn_indices.size();
+                    i++) {
+                    cout << node->henn_indices[i];
+                    if (i < node->henn_indices.size() - 1) cout << ", ";
+                }
+            cout << "}";
  
-            if (root->n == 0)
-            {
-                BTreeNode<T, ORDER> *tmp = root;
-                if (root->leaf)
-                    root = nullptr;
-                else
-                    root = root->children[0];
+            // Print HENN data information
+            if (node->henn_data) {
+                cout << "; HENN Data: [";
+                for (size_t i = 0; i < dim * node->henn_indices.size(); i++) {
+                    cout << node->henn_data[i];
+                    if (i < dim * node->henn_indices.size() - 1) cout << ", ";
+                }
+                cout << "]";
+            } else {
+                cout << "; No HENN Data";
+            }
  
-                delete tmp;
+            // Print memory address for debugging
+            cout << " @" << node << endl;
+           
+            // Recursively print children
+            if (!node->leaf) {
+                for (int i = 0; i <= node->n; i++) {
+                    string childPrefix = "Child[" + to_string(i) + "]: ";
+                    printTreeStructureHelper(node->children[i], level + 1, childPrefix);
+                }
             }
         }
+ 
     };
 }
